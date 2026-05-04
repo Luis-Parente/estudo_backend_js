d83fc3b881556fd218c35b51397036cefe69deb1
Luís Parente <parente.lgsp@outlook.com>
2026-05-04 18:19:52 -0300

    Adicionado Express ao projeto

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-import http from 'http';
+// import http from 'http';
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Node.js",
-    "/livros": "Listagem de livros",
-    "/autores": "Listagem de autores",
-}
+// const rotas = {
+//     "/": "Curso de Node.js",
+//     "/livros": "Listagem de livros",
+//     "/autores": "Listagem de autores",
+// }
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(rotas[req.url]);
-});
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end(rotas[req.url]);
+// });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log('Servidor rodando!');
 });
---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+
+const livros = [
+  { id: 1, titulo: 'O Senhor dos Anéis', autor: 'J.R.R. Tolkien' },
+  { id: 2, titulo: 'Harry Potter e a Pedra Filosofal', autor: 'J.K. Rowling' },
+  { id: 3, titulo: 'O Código Da Vinci', autor: 'Dan Brown' },
+];
+
+app.get('/', (req, res) => {
+  res.status(200).send('Curso de Node.js');
+});
+
+app.get('/livros', (req, res) => {
+  res.status(200).json(livros);
+});
+
+export default app;
